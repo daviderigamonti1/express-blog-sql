70b7a11c1b11664044028c3f05010ae5dfc5d9c1
daviderigamonti1 <davide.rigamonti.01@gmail.com>
2025-01-22 17:44:07 +0100

    implemented the show function to return a specific post in JSON format

--- controllers/exampleController.js
+++ controllers/exampleController.js
@@ -1,12 +1,5 @@
 import connection from "../connection.js"
 
-//function index(req, res) {
-//   const response = {
-//       totalCount: posts.length,
-//    data: [...posts],
-//   };
-//  res.json(response);
-//}
 function index(req, res) {
     const sql = 'SELECT * FROM posts';
     connection.query(sql, (err, results) => {
@@ -16,12 +9,13 @@ function index(req, res) {
 }
 
 function show(req, res) {
-    const id = parseInt(req.params.id);
-    const item = posts.find((item) => item.id === id);
-    if (!item) {
-        throw new CustomError("L'elemento non esiste", 404);
-    }
-    res.json({ success: true, item });
+    const id = req.params.id
+    const sql = 'SELECT * FROM posts WHERE id = ?';
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+        if (results.length === 0) return res.status(404).json({ error: 'Pizza not found' });
+        res.json(results[0]);
+    });
 }
 
 /* function store(req, res) {
